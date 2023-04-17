646d4dab670f9854783ff1a7be4a05109c9ca321
scg1704 <126414514+scg1704@users.noreply.github.com>
2023-04-17 15:55:02 -0500

    Add files via upload
    Kids location by department, invert, change extremes, error if the kid had been already added

---
+++ src/main/java/co/edu/umanizales/tads/model/City.java
@@ -0,0 +1,11 @@
+package co.edu.umanizales.tads.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class City {
+    private String code;
+    private String name;
+}
---
+++ src/main/java/co/edu/umanizales/tads/model/Kid.java
@@ -0,0 +1,16 @@
+package co.edu.umanizales.tads.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+public class Kid {
+    private String identification;
+    private String name;
+    private int age;
+    private char gender;
+    private City city;
+}
