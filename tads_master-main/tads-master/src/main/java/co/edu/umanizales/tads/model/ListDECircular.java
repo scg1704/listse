72ff2cada1a498ae0c6626df84f650bb5bc3aef0
scg1704 <scg172004@gmail.com>
2023-05-14 09:47:03 -0500

    Lógica de los métodos ya realizados

--- tads_master-main/tads-master/src/main/java/co/edu/umanizales/tads/model/ListDECircular.java
+++ tads_master-main/tads-master/src/main/java/co/edu/umanizales/tads/model/ListDECircular.java
@@ -8,6 +8,25 @@ public class ListDECircular {
     private NodeDE head;
     private int size;
 
+    /*
+    LÓGICA MÉTODO AÑADIR:
+    Entrada:
+    El animal que vamos a añadir a la lista
+
+    Agregamos el animal a un nuevo nodo
+    La lista tiene elementos?
+    SI
+        Llamamos un ayudante a que se situe en la cabeza
+        Le decimos al ayudante que recorra la lista hasta que llegue al nodo anterior a la cabeza
+        Ahora le decimos que siguiente a ese nodo ponga el nuevo nodo
+        Le decimos que el que estaba de ùltimo tome al nuevo
+        Le decimos al nuevo que tome al que estaba de último, y a la cabeza
+        Le decimos a la cabeza que tome al nuevo nodo
+    NO
+        Le decimos al nuevo nodo que se tome a si mismo
+        Le decimos que se posicione en la cabeza
+     */
+
     public void addPet (Pet pet) {
         NodeDE newPet = new NodeDE(pet);
         if (this.head != null){
@@ -27,6 +46,25 @@ public class ListDECircular {
         }
         size++;
     }
+
+    /*
+    LÓGICA MÉTODO AÑADIR AL INICIO:
+    Entrada:
+    El animal que vamos a añadir
+
+    Agregamos al animal a un nuevo nodo
+    La lista tiene elementos?
+    SI
+        Llamamos a un ayudante
+        Le decimos que recorra la lista hasta llegar al nodo anterior a la cabeza
+        Le decimos al temporal que tome al nuevo nodo siguiente a este
+        Decimos que nuevo nodo tome a temporal, y tome a cabeza
+        Decimos a cabeza que tome al nuevo nodo
+        Decimos que el nuevo nodo es la cabeza
+    NO
+        Insertamos al nuevo nodo como cabeza
+        Le decimos que se tome a si mismo
+     */
 
     public void addPetStart (Pet pet) {
         NodeDE newPet = new NodeDE(pet);
