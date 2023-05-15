cbada184a272c425a57b421dccd3c16f0072fc46
scg1704 <scg172004@gmail.com>
2023-05-14 19:08:47 -0500

    Métodos y lógica -Directo a pruebas en postman-

--- tads_master-main/tads-master/src/main/java/co/edu/umanizales/tads/controller/ListDECirclesController.java
+++ tads_master-main/tads-master/src/main/java/co/edu/umanizales/tads/controller/ListDECirclesController.java
@@ -7,10 +7,7 @@ import co.edu.umanizales.tads.service.ListDEService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path="/circular")
@@ -35,6 +32,20 @@ public class ListDECirclesController {
     public ResponseEntity<ResponseDTO> addPetStart(@RequestBody Pet pet){
         listDECirclesService.getPets().addPetStart(pet);
         return new ResponseEntity<>(new ResponseDTO(200, "the pet has been added", null),
+                HttpStatus.OK);
+    }
+
+    @GetMapping(path="/addposition")
+    public ResponseEntity<ResponseDTO> addPetPosition(@PathVariable int position, @RequestBody Pet pet){
+        listDECirclesService.getPets().addPetByPosition(pet, position);
+        return new ResponseEntity<>(new ResponseDTO(200, "The pet has been added", null),
+                HttpStatus.OK);
+    }
+
+    @GetMapping(path="/showerpet")
+    public ResponseEntity<ResponseDTO> showerPet(@PathVariable int position){
+        listDECirclesService.getPets().showerPet(position);
+        return new ResponseEntity<>(new ResponseDTO(200, "The pet is clean", null),
                 HttpStatus.OK);
     }
 }
--- tads_master-main/tads-master/src/main/java/co/edu/umanizales/tads/model/ListDECircular.java
+++ tads_master-main/tads-master/src/main/java/co/edu/umanizales/tads/model/ListDECircular.java
@@ -87,7 +87,117 @@ public class ListDECircular {
         size++;
     }
 
-    public void addPetByPosition (Pet pet, int position){
+    /*
+    LÓGICA MÉTODO AÑADIR POR POSICIÓN:
+    Entrada:
+    Mascota a añadir
+    Posición donde debe de ser añadida
 
+    La posición solicitada es menor a 0?
+    SI
+        Decimos que esto no es posible
+    Ponemos a la nueva mascota en un nuevo nodo
+    La lista está vacía?
+    NO
+        Llamamos a un ayudante
+        La posición ingresada es 0?
+        SI
+            Añadimos la nueva mascota al inicio
+            Este procedimiento se realiza de la misma forma que en el método de añadir al inicio
+            (Le decimos al ayudante que recorra la lista hasta llegar al nodo anterior a la cabeza
+            Le decimos al temporal que tome al nuevo nodo siguiente a este
+            Decimos que nuevo nodo tome a temporal, y tome a cabeza
+            Decimos a cabeza que tome al nuevo nodo
+            Decimos que el nuevo nodo es la cabeza)
+        La posición ingresada es mayor o igual al tamaño?
+        SI
+            Añadimos la nueva mascota al final
+            Este procedimiento se realiza de la misma forma que en el método añadir al final
+            (Le decimos al ayudante que recorra la lista hasta que llegue al nodo anterior a la cabeza
+            Ahora le decimos que siguiente a ese nodo ponga el nuevo nodo
+            Le decimos que el que estaba de ùltimo tome al nuevo
+            Le decimos al nuevo que tome al que estaba de último, y a la cabeza
+            Le decimos a la cabeza que tome al nuevo nodo)
+        Si no se cumplen los dos casos anteriores:
+            Recorremos las posiciones hasta llegar a la anterior a la indicada
+            Decimos que el nuevo nodo se una al que le sigue al temporal
+            Decimos al nuevo nodo que se una al temporal
+            Decimos al siguiente del temporal que tome el nuevo nodo
+            Decimos al temporal que tome el nuevo nodo
+    SI
+        Ponemos como cabeza al nuevo nodo
+        Le decimos que se tome a si mismo
+     */
+
+    public void addPetByPosition (Pet pet, int position){
+        if (position < 0){
+            //Esto lo investigué, y nos dice que es algo no posible, por lo tanto no hará cambios
+            throw new IllegalArgumentException("ERROR");
+        }
+        NodeDE newPet = new NodeDE(pet);
+        if (this.head != null){
+            NodeDE temp = this.head;
+            if (position == 0) {
+                while (temp.getNext() != this.head){
+                    temp = temp.getNext();
+                }
+                temp.setNext(newPet);
+                newPet.setPrevious(temp);
+                newPet.setNext(head);
+                head.setPrevious(newPet);
+                this.head=newPet;
+            }
+            else if (position >= size){
+                while (temp.getNext() != this.head){
+                    temp = temp.getNext();
+                }
+                temp.setNext(newPet);
+                newPet.setPrevious(temp);
+                newPet.setNext(this.head);
+                this.head.setPrevious(newPet);
+            }
+            else{
+                for (int i = 0; i < position -1; i++){
+                    temp = temp.getNext();
+                }
+                newPet.setNext(temp.getNext());
+                newPet.setPrevious(temp);
+                temp.getNext().setPrevious(newPet);
+                temp.setNext(newPet);
+            }
+        }
+        else{
+            newPet.setNext(newPet);
+            newPet.setPrevious(newPet);
+            this.head = newPet;
+        }
+        size++;
+    }
+
+    /*
+    NOTA: A mí forma de entender el ejercicio, aquello que se debe de hacer es bañar la mascota.
+    Para ello le añadí el atributo booleano de shower, y si este es false, muestra que la mascota no ha sido
+    bañada, y si es true, muestra que ha sido bañada. Aquello que se nos pidió fue un método que bañara a las
+    mascotas, según la que posición que fue solicitada, y dado el caso de que la posición fuese mayor a la lista
+    entonces seguiamos recorriendo la lista.
+
+    LÓGICA MÉTODO BAÑAR MASCOTA:
+    Entrada:
+    Posición de la mascota que queremos bañar
+
+    La lista tiene datos?
+    SI
+        Llamamos a un ayudante
+        Recorremos las posiciones hasta llegar a la anterior a la indicada
+        Decimos que tome los datos del siguiente (Que es la posición indicada) y lo bañe
+     */
+    public void showerPet (int position){
+        if (this.head != null){
+            NodeDE temp = this.head;
+            for (int i = 0; i < position -1; i++){
+                temp = temp.getNext();
+            }
+            temp.getData().setShower(true);
+        }
     }
 }
